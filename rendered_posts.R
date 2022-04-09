4b54ea8ab58128a79ff0d4fab147620964f44ce6
Christopher Maronga <marongachristopher@gmail.com>
2022-04-09 23:31:54 +0100

    post one

---
+++ rendered_posts.R
@@ -0,0 +1,5 @@
+# Web scraping using R (2022-04-08)
+build_blogPost(date = "2022-04-08", 
+               folder_name = "web-scraping-in-R", 
+               rmd_full_name = "Web scraping using R.Rmd" , 
+               post_name = "Web scraping using-R")
