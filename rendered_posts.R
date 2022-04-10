80a891efd3320c84cf311f274dd5866271511908
Christopher Maronga <marongachristopher@gmail.com>
2022-04-10 01:21:16 +0100

    update database with RÂÂ

--- rendered_posts.R
+++ rendered_posts.R
@@ -3,3 +3,10 @@ build_blogPost(date = "2022-04-08",
                folder_name = "web-scraping-in-R", 
                rmd_full_name = "Web scraping using R.Rmd" , 
                post_name = "Web scraping using-R")
+
+
+# Working with databases in R (2021-06-12)
+build_blogPost(date = "2021-06-12", 
+               folder_name = "databases-with-R", 
+               rmd_full_name = "working-with-databases-in-R.Rmd" , 
+               post_name = "Working with databases in-R")
